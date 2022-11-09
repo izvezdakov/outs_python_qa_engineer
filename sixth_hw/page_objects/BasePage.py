c33177bbf6a776bf28bb9a071042989ae26f25a5
izvezdakov@ozon.ru <izvezdakov@ozon.ru>
2022-11-09 18:49:00 +0300

    add allure steps

--- sixth_hw/page_objects/BasePage.py
+++ sixth_hw/page_objects/BasePage.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import allure
 
 
 class BasePage:
@@ -20,13 +21,15 @@ class BasePage:
 
     def click(self, element):
         self.logger.info(f'Clicking element: {element}')
-        ActionChains(self.driver).move_to_element(element).pause(0.1).click().perform()
+        with allure.step(f'click on element {element}'):
+            ActionChains(self.driver).move_to_element(element).pause(0.1).click().perform()
 
     def input(self, element, value):
         self.logger.info(f'Input {value} in {element}')
-        self.click(element)
-        element.clear()
-        element.send_keys(value)
+        with allure.step(f'input {value} in {element}'):
+            self.click(element)
+            element.clear()
+            element.send_keys(value)
 
     def element_in_element(self, parent_locator: tuple, child_locator: tuple):
         return self.element(parent_locator).find_element(*child_locator)
@@ -43,6 +46,9 @@ class BasePage:
         try:
             return WebDriverWait(self.driver, 5).until(EC.visibility_of_all_elements_located(locator))
         except TimeoutException:
+            allure.attach(body=self.driver.get_screenshot_as_png,
+                          attachment_type=allure.attachment_type.PNG,
+                          name='screenshot_image')
             raise AssertionError(f"Не дождался видимости элементов {locator}")
 
     def verify_product_item(self, product_name):
